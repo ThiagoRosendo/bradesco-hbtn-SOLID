92f8deaf18a3cd1a20d55af74a969c0b150af386
Thiago Rosendo <thiago_953@hotmail.com>
2025-10-14 22:13:48 +0000

    2. Convenções de Nomenclatura

---
+++ 0x04/convencao/UserModel.java
@@ -0,0 +1,11 @@
+class UsersModels {
+
+    private int idUser;
+    
+    private String descricao;
+    
+    final static int IDADE_MINIMA = 18;
+    
+    public getUsers() {return [];}
+    
+    }
